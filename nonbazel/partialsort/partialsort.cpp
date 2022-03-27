86028a622d05b4e8e6d552df3b8dddaae69087ea
Lennart Moltrecht <lmoltrecht@nvidia.com>
2022-03-27 18:38:58 +0800

    Move timeit to utils under bazel

--- nonbazel/partialsort/partialsort.cpp
+++ nonbazel/partialsort/partialsort.cpp
@@ -1,11 +1,13 @@
 #include <algorithm>
-#include <chrono>
 #include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <limits>
 #include <random>
+#include <utility>
 #include <vector>
+
+#include "../../utils/timeit.h"
 
 /**
  * @brief Get vector with random data
@@ -15,8 +17,7 @@
  * @param seed seed for the random number generator
  * @return std::vector<T> vector of random elements
  */
-template <class T>
-std::vector<T> get_data(std::size_t size, std::size_t seed = 42U)
+template <class T> std::vector<T> get_data(std::size_t size, std::size_t seed = 42U)
 {
     // Seed engine with seed and size so that the vectors of different size don't all start with the same numbers
     std::mt19937 engine(seed * size);
@@ -33,61 +34,39 @@ std::vector<T> get_data(std::size_t size, std::size_t seed = 42U)
 }
 
 /**
- * @brief Measure wall time of function passed as argument
- *
- * @tparam Fcn
- * @param f function pointer or function object to be measured
- * @return double wall time of execution in milliseconds
- */
-template <class Fcn>
-double time_it(Fcn f)
-{
-    auto const start = std::chrono::high_resolution_clock::now();
-
-    f();
-
-    auto const end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration<double, std::milli>(end - start).count();
-}
-
-/**
- * @brief Run benchmark for given type and size
+ * @brief Run benchmark with all sorting methods for given type and size
  *
  * @tparam ElementType type of the elements to be sorted
  * @param buffer_size number of elements
  */
-template <class ElementType>
-void run(std::size_t buffer_size)
+template <class ElementType> void run_benchmark(std::size_t buffer_size)
 {
-    std::cerr << "Requiring " << static_cast<double>(sizeof(ElementType) * buffer_size) / 1024. / 1024. << " MB for "
+    std::cerr << "[I] Requiring " << static_cast<double>(sizeof(ElementType) * buffer_size) / 1024. / 1024. << " MB for "
               << buffer_size << " elements of type " << typeid(ElementType).name() << std::endl;
 
     const double fraction{0.1};
-    double duration_sort;
-    double duration_partial_sort;
-    double duration_nth_element_sort;
 
-    {
+    double const duration_sort = [buffer_size] {
         auto data{get_data<ElementType>(buffer_size)};
-        duration_sort = time_it([&data]() { std::sort(begin(data), end(data)); });
-    }
+        return timeit([&data]() { std::sort(begin(data), end(data)); });
+    }();
 
-    {
+    double const duration_partial_sort = [buffer_size, fraction] {
         auto data{get_data<ElementType>(buffer_size)};
         auto cutoff = begin(data) + fraction * data.size();
-        duration_partial_sort = time_it([&data, &cutoff]() { std::partial_sort(begin(data), cutoff, end(data)); });
-    }
+        return timeit([&data, &cutoff]() { std::partial_sort(begin(data), cutoff, end(data)); });
+    }();
 
-    {
+    double const duration_nth_element_sort = [buffer_size, fraction] {
         auto data{get_data<ElementType>(buffer_size)};
         auto cutoff = begin(data) + fraction * data.size();
-        duration_nth_element_sort = time_it([&data, &cutoff]() {
+        return timeit([&data, &cutoff]() {
             std::nth_element(begin(data), cutoff, end(data));
             std::sort(begin(data), cutoff);
         });
-    }
+    }();
 
-    std::cerr << "# buffer_size, sort, partialsort, nthelement" << std::endl;
+    std::cerr << "[I] Printing results: buffer_size, sort, partialsort, nthelement" << std::endl;
     std::cout << buffer_size << ", " << duration_sort << ", " << duration_partial_sort << ", "
               << duration_nth_element_sort << std::endl;
 }
@@ -115,23 +94,23 @@ int main(int argc, const char* argv[])
     // Unfortunately cannot switch on string in C++
     if (datatype == "int8")
     {
-        run<std::int8_t>(buffer_size);
+        run_benchmark<std::int8_t>(buffer_size);
     }
     if (datatype == "int16")
     {
-        run<std::int16_t>(buffer_size);
+        run_benchmark<std::int16_t>(buffer_size);
     }
     else if (datatype == "int32")
     {
-        run<std::int32_t>(buffer_size);
+        run_benchmark<std::int32_t>(buffer_size);
     }
     else if (datatype == "int64")
     {
-        run<std::int64_t>(buffer_size);
+        run_benchmark<std::int64_t>(buffer_size);
     }
     else
     {
-        std::cerr << "Unsupported datatype '" << datatype << "'! Aborting..." << std::endl;
+        std::cerr << "[E] Unsupported datatype '" << datatype << "'! Aborting..." << std::endl;
         return 1;
     }
 
